b6a94f66354ea0ea32b12f9c9665e158e0f0691a
sayanastha39 <54332268+sayanastha39@users.noreply.github.com>
2020-01-29 11:46:06 -0500

    Add files via upload

---
+++ CodingChallenge/src/challenge2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class challenge2 {
+
+	public static void main(String[] args) {
+		
+		List<String> lastNames = Arrays.asList("Adams", "Quincy", "Washington", "Allison", "Jones", "Ward", "Johnson", "Barnes", "Calvin", "Zigler", "Ball");
+		
+		Collections.sort(lastNames);
+		System.out.println("Array of last names sorted in ascending order :" + lastNames);
+		
+		for (String sorted: lastNames) {
+			
+			if(sorted.startsWith("W")) {
+				
+				int index = lastNames.indexOf(sorted);
+				
+				System.out.println(index + "  is the index of  " + sorted + ",   which is the first last name that starts with letter 'W' ");
+				
+				break;
+			}
+		}
+		
+	}
+
+}
---
+++ CodingChallenge/src/sortingAlgorithm.java
@@ -0,0 +1,44 @@
+//Bubble Sort
+public class sortingAlgorithm {
+
+	public static void main(String[] args) {
+		
+		String[] lastNames = {"Adams", "Quincy", "Washington", "Allison", "Jones", "Ward", "Johnson", "Barnes", "Calvin", "Zigler", "Ball"};
+		 
+		String sorted;
+        int len = lastNames.length;
+        
+        for (int i = 0; i < len; i++) 
+        {
+        	for (i = 0; i < len-1; i++) {
+        		
+	            for (int j = 0; j < len-i-1; j++) 
+	            {
+	                if (lastNames[j].compareTo(lastNames[j+1])>0) 
+	                {
+	                	sorted = lastNames[j];
+	                    lastNames[j] = lastNames[j+1];
+	                    lastNames[j+1] = sorted;
+	                }
+	            }
+	        }
+        }
+        
+        System.out.println("Last names sorted in ascending order :");
+	        
+        for (int i = 0; i < len; i++){
+        	System.out.print(lastNames[i] + ", ");
+	    }
+        
+        System. out. println();
+        
+        for (int i = 0; i < len; i++){
+        	if (lastNames[i].charAt(0) == 'W') {
+        		System.out.println("Index of first occurence of last name that starts with letter 'W' is: " + i);
+        		break;
+        	}
+        }
+    }
+}
+
+
